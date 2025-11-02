963fca377406c921e42e85f8382a29983e2a1e8d
Hashan Galagedara <rnd2@cenmetrix.lk>
2025-11-02 13:22:54 +0530

    Config change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cenhashan.github.io/hashan.online',
+    site: 'https://cenhashan.github.io',
     base: '/hashan.online',
 });
